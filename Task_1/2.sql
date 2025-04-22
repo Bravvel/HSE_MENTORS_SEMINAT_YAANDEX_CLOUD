c285d2fdb397c89a2bc13dfe7a8f3bc5f052afbf
Bravvel <120821657+Bravvel@users.noreply.github.com>
2025-04-22 08:44:27 -0700

    Add files via upload

---
+++ Task_1/2.sql
@@ -0,0 +1,6 @@
+SELECT is_fraud,
+  COUNT(*) AS transaction_count,
+  SUM(amount) AS total_amount,
+  ROUND(AVG(amount),2) AS avg_amount
+FROM transactions_v2
+GROUP BY is_fraud;
---
+++ Task_1/3.sql
@@ -0,0 +1,9 @@
+SELECT
+  DATE(transaction_date) AS day,
+  COUNT(*) AS transaction_count,
+  SUM(amount) AS total_amount,
+  ROUND(AVG(amount),2) AS avg_amount
+FROM transactions_v2
+GROUP BY DATE(transaction_date)
+ORDER BY day;
+
